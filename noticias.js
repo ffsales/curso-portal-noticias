0141d5230fc73b943fe7c617cee8686b7d707674
Felipe Sales <felipe.ferreira.sales@gmail.com>
2018-02-02 15:40:06 -0200

    incorporando servidor http

--- noticias.js
+++ noticias.js
@@ -1,1 +1,9 @@
-console.log('Criando um site de noticias com Nodejs.');
+var http = require('http');
+
+var server = http.createServer( function(req, res) {
+
+	res.end("<html><body>Portal de noticias</body></html>");
+
+} );
+
+server.listen(3000);
